5336f8990710dfc22dda4de60d1507cb26841da8
Luke Kerbs <luke@Lukes-MBP-3.lan1>
2021-01-19 18:47:31 -0800

    added example menu image

--- menu_display.py
+++ menu_display.py
@@ -1,35 +1,25 @@
-import cv2
-import time
+import pygame
+from pygame.locals import *
 import screeninfo
 
-def showImage(filename):
-	screen_id = 0
-	screen = screeninfo.get_monitors()[screen_id]
-	W, H = screen.width, screen.height
+screen_id = 0
+screen = screeninfo.get_monitors()[screen_id]
+W, H = screen.width, screen.height
 
-	raw_img = cv2.imread(filename)
-	height, width, depth = raw_img.shape
+print('width', W)
+print('height', H)
 
-	scaleWidth = float(W)/float(width)
-	scaleHeight = float(H)/float(height)
 
-	if scaleHeight > scaleWidth:
-		imgScale = scaleWidth
-	else:
-		imgScale = scaleHeight
-	
-	newX, newY = raw_img.shape[1]*imgScale, raw_img.shape[0]*imgScale
-	resized_img = cv2.resize(raw_img, (int(newX),int(newY)))
-	window_name = 'projector'
-	cv2.namedWindow(window_name, cv2.WND_PROP_FULLSCREEN)
-	cv2.setWindowProperty(window_name, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
-	cv2.imshow(window_name, resized_img)
-	time.sleep(1) 
-	cv2.waitKey(1) 
-	cv2.destroyAllWindows()
-
-files = ['imgs/1.png', 'imgs/2.png', 'imgs/3.png', 'imgs/4.png']
-
-for i in range(5):
-	for j in range(len(files)):
-		showImage(files[j])
+pygame.init()
+WIDTH = W
+HEIGHT = H
+windowSurface = pygame.display.set_mode((WIDTH, HEIGHT), pygame.NOFRAME)
+img = pygame.image.load("imgs/1.png")
+while True:
+        events = pygame.event.get()
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+        windowSurface.blit(img, (0, 0)) #Replace (0, 0) with desired coordinates
+        pygame.display.flip()
