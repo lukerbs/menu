d7661c99c920592fa60778fb5b32296d3f3a53de
Luke Kerbs <luke@Lukes-MBP-3.lan1>
2021-01-24 13:56:21 -0800

    added images and weekday menu

--- menu_display.py
+++ menu_display.py
@@ -1,6 +1,8 @@
 import pygame
 from pygame.locals import *
 import screeninfo
+import datetime
+import time
 
 screen_id = 0
 screen = screeninfo.get_monitors()[screen_id]
@@ -9,17 +11,28 @@ W, H = screen.width, screen.height
 print('width', W)
 print('height', H)
 
-
 pygame.init()
 WIDTH = W
 HEIGHT = H
 windowSurface = pygame.display.set_mode((WIDTH, HEIGHT), pygame.NOFRAME)
-img = pygame.image.load("imgs/5.png")
+
+weekday_menu = ['imgs/hotdog.png', 'imgs/nacho.png', 'imgs/potato.png', 'imgs/taco.png']
+weekend_menu = ['imgs/hotdog.png', 'imgs/nacho.png', 'imgs/potato.png', 'imgs/taco.png', 'imgs/menudo.png', 'imgs/pazole.png', 'imgs/conchas.png']
+
 while True:
-        events = pygame.event.get()
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                pygame.quit()
-                sys.exit()
-        windowSurface.blit(img, (0, 0)) #Replace (0, 0) with desired coordinates
-        pygame.display.flip()
+	today = datetime.datetime.today().weekday()
+	# 0 = Monday, 1 = Tuesday, ..., 6 = Sunday
+	if today == 5 or today == 6:
+		menu = weekend_menu
+	else:
+		menu = weekday_menu
+	for i in range(len(menu)):
+		img = pygame.image.load(menu[i])
+		events = pygame.event.get()
+		for event in pygame.event.get():
+			if event.type == QUIT:
+				pygame.quit()
+				sys.exit()
+		windowSurface.blit(img, (0, 0)) #Replace (0, 0) with desired coordinates
+		pygame.display.flip()
+		time.sleep(5)
